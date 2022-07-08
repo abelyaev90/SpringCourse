8adce985819237337e2c572ea366f33e782d09cf
aleksandrbelaev <alexanderbelyaev1990@gmail.com>
2022-07-08 17:45:01 +0200

    11 lesson

--- src/main/java/ru/belyaev/springcourse/MusicPlayer.java
+++ src/main/java/ru/belyaev/springcourse/MusicPlayer.java
@@ -1,52 +1,20 @@
 package ru.belyaev.springcourse;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-    public Music music;
-    private List<Music> musicList = new ArrayList<>();
-    private String name;
-    private int volume;
+private Music music1;
+private Music music2;
 
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
+    public MusicPlayer(@Qualifier("popMusic") Music music1, @Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void ListPlayMusic() {
-       for (Music music : musicList) {
-           System.out.println("Playing : " + music.getSong());
-       }
-    }
-
-    public void playMusic() {
-        System.out.println("Playing : " + music.getSong());
+    public String playMusic() {
+        return "Playing : " + music1.getSong() + " , " + music2.getSong();
     }
 }
--- src/main/java/ru/belyaev/springcourse/TestSpring.java
+++ src/main/java/ru/belyaev/springcourse/TestSpring.java
@@ -6,9 +6,12 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("popMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
+/*        Music music1 = context.getBean("rockMusic", Music.class);
+        Music music2 = context.getBean("popMusic", Music.class);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();*/
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
